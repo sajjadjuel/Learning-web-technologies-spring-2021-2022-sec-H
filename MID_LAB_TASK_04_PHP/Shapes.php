01e56440bb4861b9f41626e9a01a2adff9c0e702
sajjadjuel <63725506+sajjadjuel@users.noreply.github.com>
2022-02-08 14:15:25 +0600

    .

--- MID_LAB_TASK_04_PHP/Shapes.php
+++ MID_LAB_TASK_04_PHP/Shapes.php
@@ -4,7 +4,7 @@
 	{
 		for($j=$i; $j <=2*$i-1 ; $j++)
 		{
-			echo "*";
+			echo "* ";
 		}
 		echo "<br>";
 	}
@@ -12,5 +12,25 @@
 
 	echo "Shape-2 <br>";
 	
+	for($i=0; $i <3 ; $i++)
+	{
+		for($j=1; $j <=3-$i ; $j++)
+		{
+			echo "$j ";
+		}
+		echo "<br>";
+	}
+
+	echo "<br><br><br>";
+
+	echo "Shape-2 <br>";
+	$Let='A';
+	for($i=1; $i <=3 ; $i++)
+	{
+		for($j=$i; $j <=2*$i-1 ; $j++)
+		{
+			echo $Let++." ";
+		}
+		echo "<br>";
 	}
 ?>
