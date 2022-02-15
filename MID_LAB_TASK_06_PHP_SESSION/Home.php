75c7bc1d3b66b9761302b895b561426d6b78a6f8
sajjadjuel <63725506+sajjadjuel@users.noreply.github.com>
2022-02-15 13:56:55 +0600

    initial

---
+++ MID_LAB_TASK_06_PHP_SESSION/Home.php
@@ -0,0 +1,31 @@
+<?php 
+	?>
+
+<html>
+<head>
+	<title>First HTML Site</title>
+</head>
+<body>
+<pre>
+
+		<table border="1" width="50%" align="center">
+			<tr height="80px">
+				<td align="left"> <h1>XCompany</h1></td>
+				<td align="right" colspan="2"> 
+					<a href="home.php">Home|</a>
+					<a href="">Login|</a>
+					<a href="Reg.php">Registration</a>
+				</td>
+			</tr>
+			<tr height="200px">
+				<td colspan="3" align="left"><h2><pre>  Welcome to xCompany</pre></h2>
+				
+				</td>
+			</tr>
+			<tr height="50px">
+				<td colspan="3" align="center"> copyright @ 2017</td>
+			</tr>
+		</table></pre>
+</body>
+</html>
+
---
+++ MID_LAB_TASK_06_PHP_SESSION/Reg.php
@@ -0,0 +1,28 @@
+<?php 
+	?>
+
+<html>
+<head>
+	<title>First HTML Site</title>
+</head>
+<body>
+<pre>
+
+		<table border="1" width="50%" align="center">
+			<tr height="80px">
+				<td align="left"> <h1>XCompany</h1></td>
+				<td align="right" colspan="2"> 
+					<a href="home.php">Home|</a>
+					<a href="">Login|</a>
+					<a href="">Registration</a>
+				</td>
+			</tr>
+			<tr height="300px">
+			</tr>
+			<tr height="50px">
+				<td colspan="3" align="center"> copyright @ 2017</td>
+			</tr>
+		</table></pre>
+</body>
+</html>
+
